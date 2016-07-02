d524ae711ac76ec76d436630c0608ae929babaf4
Michael Colon <michaelcolon@youaredreaminginspacewheretimeisjustenergy.local>
2016-07-02 14:05:45 -0400

    polygon colors, opacity, deletion. Same for lines. marker delete only.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var sockio = require("socket.io");
+var fs = require("fs");
 var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
 var io = sockio.listen(app.listen(4200), {log:true});
+
+app.get('/api/markers', function (req, res) {
+  fs.readdir("public/images/markers", function(err, items) {
+    res.send(JSON.stringify(items));
+  });
+});
